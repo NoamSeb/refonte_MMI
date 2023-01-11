1fdb920541dc3785c88f9fc0d77b304e9bf06113
Amel Chabah <amelou518@gmail.com>
2023-01-11 16:53:02 +0100

    footer

--- gallery.php
+++ gallery.php
@@ -106,30 +106,35 @@ $result = $stmt->fetchall(PDO::FETCH_ASSOC);
     <footer>
         <div class="footer">
             <div class="map">
-                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2626.1005349879015!2d2.58308131562675!3d48.837221010151026!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47e60e33dd9a3fdd%3A0x7e5ced48ab7fc8df!2sIUT%20de%20Marne-la-Vall%C3%A9e%20-%20Universit%C3%A9%20Gustave%20Eiffel!5e0!3m2!1sfr!2sfr!4v1672012216586!5m2!1sfr!2sfr"
-                    width="400" height="200" style="border:0; border-radius: 30px;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2626.1005349879015!2d2.58308131562675!3d48.837221010151026!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47e60e33dd9a3fdd%3A0x7e5ced48ab7fc8df!2sIUT%20de%20Marne-la-Vall%C3%A9e%20-%20Universit%C3%A9%20Gustave%20Eiffel!5e0!3m2!1sfr!2sfr!4v1672012216586!5m2!1sfr!2sfr" style="border:0; border-radius: 30px;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
             </div>
             <div class="campus">
-                <p class="footerTitle">Campus</p>
-                <p class="footerTxt">2 rue Albert Einstein<br> 77420 Champs-sur-Marne<br> RER A - Noisy-Champs<br> Bus 212, 213 & 312 - Nobel
-                </p>
+                <h3>Campus</h3>
+                <ul>
+                    <li>2 rue Albert Einstein</li>
+                    <li>77420 Champs Sur Marne</li>
+                    <li>RER A - Noisy-Champs</li>
+                    <li>Bus 212, 213 & 312 - Nobel</li>
+                </ul>
             </div>
             <div class="contact">
-                <p class="footerTitle">Contact</p>
-                <p class="footerTxt">01 60 95 85 90<br> mmi-dir.iut@univ-eiffel.fr
-                </p>
-                <div class="icons">
-                    <img src="medias/insta_icon.svg" alt="Page Instagram">
-                    <img src="medias/twitter_icon.svg" alt="Page Twitter">
+                <h3>Contact</h3>
+                <ul>
+                    <li>01 60 95 85 90</li>
+                    <li> mmi-dir.iut@univ-eiffel.fr</li>
+                </ul>
+                <div class="socials">
+                    <a href="https://www.instagram.com/mmi_champs/" target="_blank"><img src="medias/insta_icon.svg" alt="Page Instagram"></a>
+                    <a href="https://twitter.com/mmi_champs" target="_blank"><img src="medias/twitter_icon.svg" alt="Page Twitter"></a>
+                    
                 </div>
             </div>
         </div>
-        <div class="bottomFooter">
-            <p>2023 - BUT Métiers du Multimédia & de l’Internet, Champs-sur-marne © Tous droits réservés</p>
-            <p class="linkMentions">MENTIONS LÉGALES</p>
+        <div class="credits">
+            <p>2023&#160;&#160;&#160;-&#160;&#160;&#160;BUT Métiers du Multimédia & de l’Internet, Champs-sur-Marne © Tous droits réservés</p>
+            <h3>mentions légales</h3>
         </div>
 
-    </footer>
-</body>
+    </footer></body>
 
 </html>
