7402a9df5605f2490eaad6f2f2bf5e0f3e3f1149
NoamSeb <noam.sebahoun@gmail.com>
2023-01-11 19:29:27 +0100

    add style to affiche-event.php

--- affiche-event.php
+++ affiche-event.php
@@ -11,6 +11,7 @@ $result = $stmt->fetchall(PDO::FETCH_ASSOC);
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
 <div class="menu-overlay">
