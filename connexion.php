1802dc9b0ffe2f704941a1682d10a89a41b0000e
eleaacrt <eleaa.crt@gmail.com>
2023-01-11 16:56:53 +0100

    slider + bdd

--- connexion.php
+++ connexion.php
@@ -1,3 +1,3 @@
 <?php
-$db = new PDO('mysql:host=localhost;dbname=refonte_mmi;port:3306', 'root', 'root');
+$db=new PDO('mysql:host=localhost; dbname=refonte_mmi; charset=utf8; port=3306','root','');
 ?>
