cef08416ee98393174c9fee7cfd1c2431b01ff78
Amel Chabah <amelou518@gmail.com>
2023-01-16 23:15:29 +0100

    oups

--- SQL/modificationSQL/0116_2312_amel_create.sql
+++ SQL/modificationSQL/0116_2312_amel_create.sql
@@ -1,1 +1,1 @@
-CREATE TABLE actus (id_actu INT PRIMARY KEY NOT NULL, titre_actu VARCHAR(200), description_actu TEXT, iage_actu TEXT);
+CREATE TABLE actus (id_actu INT PRIMARY KEY NOT NULL, titre_actu VARCHAR(200), description_actu TEXT, image_actu TEXT);
