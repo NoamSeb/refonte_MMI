b74f561354dd0145fd169e1278c8172ad6e080da
Amel Chabah <amelou518@gmail.com>
2023-01-16 23:43:34 +0100

    test tableau

--- admin/evenement.php
+++ admin/evenement.php
@@ -15,7 +15,7 @@ if (!isset($_SESSION["login"])) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link rel="stylesheet" href="../style.css">
-    <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous"> -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
 </head>
 
 <body>
@@ -57,14 +57,15 @@ if (!isset($_SESSION["login"])) {
                 </thead>
                 <tbody>
                     <?php
-                    foreach ($events as $event) { ?>
+                    foreach ($events as $value) { ?>
                         <tr>
-                            <td><?= $event['id_event'] ?></td>
-                            <td><?= $event['nom_event'] ?></td>
-                            <td><?= mb_strimwidth($event['description_event'], 0, 200, "...") ?></td>
-                            <td><?= $event['date_event'] ?></td>
-                            <td><?= $event['img_event'] ?></td>
-                            <td></td>
+                            <td><?= $value['id_event'] ?></td>
+                            <td><?= $value['nom_event'] ?></td>
+                            <td><?= mb_strimwidth($value['description_event'], 0, 200, "...") ?></td>
+                            <td><?= $value['date_event'] ?></td>
+                            <td><?= $value['img_event'] ?></td>
+                            <td><?=
+                                '<div style="display:flex; gap:1rem"><a type="button" class="btn btn-xs btn-info" href="read.php?billet=' . $value['id_event'] . '" >&#128065;</a><a type="button" class="btn btn-xs btn-warning" href="edit.php?action=edit & id=' . $value['id_event'] . '">&#9998;</a><a type="button" class="btn btn-xs btn-danger" onclick="return confirm(`Êtes-vous sûr de vouloir supprimer l\'article ?`)" href="delete.php?type=billet&delete & id=' . $value['id_event'] . '">&#128465;</a></div>'; ?></td>
                         </tr>
                     <?php } ?>
                 </tbody>
@@ -72,7 +73,7 @@ if (!isset($_SESSION["login"])) {
         </div>
     </div>
 </body>
-<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.min.js" integrity="sha384-cuYeSxntonz0PPNlHhBs68uyIAVpIIOZZ5JqeqvYYIcEL727kskC66kF92t6Xl2V" crossorigin="anonymous"></script>
+<script src=" https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
+                                        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.min.js" integrity="sha384-cuYeSxntonz0PPNlHhBs68uyIAVpIIOZZ5JqeqvYYIcEL727kskC66kF92t6Xl2V" crossorigin="anonymous"></script>
 
 </html>
