3e30d1d22e7a29f105353048aa918b2930085868
NoamSeb <noam.sebahoun@gmail.com>
2023-01-18 03:50:25 +0100

    rename pages for the back office

--- admin/addevent.php
+++ admin/addevent.php
@@ -13,7 +13,7 @@ if (!isset($_SESSION["login"])) {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Back Office</title>
     <link rel="stylesheet" href="../style.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
 </head>
